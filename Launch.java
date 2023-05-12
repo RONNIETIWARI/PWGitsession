a6e47e735faa7f7b2594d4535a09212cce8800f5
RONNIETIWARI <tiwari.subodh.k20@gmail.com>
2023-05-12 17:23:45 +0530

    launch.java is modified

--- Launch.java
+++ Launch.java
@@ -2,6 +2,10 @@ class launch
 {
    void m1()
    {
-       //some code
+       System.out.println("hello Java lovers")
+   }
+   void m2()
+   {
+       System.out.println("hello Java lovers")
    }
 }
